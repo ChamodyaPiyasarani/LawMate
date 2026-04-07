30fdf33f2110a147d760d56f9aa9f960a7db9e6c
ChamodyaPiyasarani <chamo.piyasarani2002@gmail.com>
2026-04-07 10:17:27 +0530

    initial commit

---
+++ LawMate/LawMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LawMateApp.swift
+//  LawMate
+//
+//  Created by COBSCCOMP242P-030 on 2026-04-07.
+//
+
+import SwiftUI
+
+@main
+struct LawMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
